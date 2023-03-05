a3d67f65aee7acc9bab53ed561bd2530dbbee031
Phat Nguyen <tanphat199@gmail.com>
2023-03-05 19:40:01 +0700

    edit message error

--- src/repositories/migration.repository.ts
+++ src/repositories/migration.repository.ts
@@ -2,11 +2,10 @@ import { inject } from '@loopback/core';
 import { Migration } from '@/models';
 import { TimestampCrudRepository, BaseDataSource } from '@/base';
 import { isEmpty } from 'lodash';
-import { getError } from '..';
 
 const migrationDs = process.env.DS_MIGRATION;
 if (!migrationDs || isEmpty(migrationDs)) {
-  throw getError({ message: `[DANGER] INVALID MIGRATION DATASOURCE | Check again env DS_MIGRATION` });
+  throw new Error('[DANGER] INVALID MIGRATION DATASOURCE | Check again env DS_MIGRATION');
 }
 
 export class MigrationRepository extends TimestampCrudRepository<Migration> {
