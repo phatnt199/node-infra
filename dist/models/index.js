d3f604feae99dd2dd1ed1fd603de1c542ab406ad
Phat Nguyen <tanphat199@gmail.com>
2023-03-19 15:28:08 +0700

    fix

--- dist/models/index.js
+++ dist/models/index.js
@@ -14,10 +14,10 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("./migration.model"), exports);
 __exportStar(require("./user.model"), exports);
 __exportStar(require("./role.model"), exports);
 __exportStar(require("./permission.model"), exports);
 __exportStar(require("./user-mapping.model"), exports);
 __exportStar(require("./permission-mapping.model"), exports);
+__exportStar(require("./migration.model"), exports);
 //# sourceMappingURL=index.js.map
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,6 @@
-export * from './migration.model';
 export * from './user.model';
 export * from './role.model';
 export * from './permission.model';
 export * from './user-mapping.model';
 export * from './permission-mapping.model';
+export * from './migration.model';
