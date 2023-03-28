a6a9ed91b02c23275403e9abf885ddc587386c5e
Phat Nguyen <tanphat199@gmail.com>
2023-03-29 00:38:36 +0700

    fix wrong formatter

--- src/components/authenticate/jwt.strategy.ts
+++ src/components/authenticate/jwt.strategy.ts
@@ -9,7 +9,7 @@ export class JWTAuthenticationStrategy implements AuthenticationStrategy {
 
   constructor(@inject('services.JWTTokenService') private service: JWTTokenService) { }
 
- async authenticate(request: Request) {
+  async authenticate(request: Request) {
     const token = this.service.extractCredentials(request);
     return this.service.verify(token);
   }
