b2ffa1829cef4e5205c533056bec96bac91b42af
Phat Nguyen <tanphat199@gmail.com>
2024-07-05 07:30:41 +0700

    v0.0.8

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -3,6 +3,6 @@
 echo "Clean up ...START"
 ./node_modules/.bin/lb-clean dist *.tsbuildinfo .eslintcache
 
-rm -rf  artifact.zip
+rm -rf artifact.zip
 
 echo "Clean up ...DONE"
---
+++ scripts/rebuild.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+provision_opt=$1
+case "$provision_opt" in
+  "--major") yarn version $provision_opt ;;
+  "--minor") yarn version $provision_opt ;;
+  "--patch") yarn version $provision_opt ;;
+  "--no-version") echo "No versioning for build!" ;;
+  *)
+    echo "Invalid versioning opts | Valid: [major | minor | patch | no-version]"
+    exit 1
+    ;;
+esac
+
+echo "\nCleaning up resources ..."
+yarn clean
+
+echo "\nBuilding latest release..."
+yarn build
+
+echo "\nPlease push latest build"
