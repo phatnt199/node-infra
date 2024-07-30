fab033e0cf9bbccaddf66e8413502a4bcd5001b3
Phat Nguyen <tanphat199@gmail.com>
2024-07-30 16:03:43 +0700

    remove default method for casbin adapter

--- src/components/authorize/adapters/base.adapter.ts
+++ src/components/authorize/adapters/base.adapter.ts
@@ -1,4 +1,5 @@
 import { BaseDataSource } from '@/base';
+import { EnforcerDefinitions } from '@/common';
 import { ApplicationLogger, LoggerFactory } from '@/helpers';
 import { FilteredAdapter, Model } from 'casbin';
 
@@ -15,15 +16,57 @@ export abstract class AbstractCasbinAdapter implements FilteredAdapter {
   }
 
   abstract loadFilteredPolicy(model: Model, filter: any): Promise<void>;
-  abstract isFiltered(): boolean;
-  abstract loadPolicy(model: Model): Promise<void>;
-  abstract savePolicy(model: Model): Promise<boolean>;
-  abstract addPolicy(sec: string, ptype: string, rule: string[]): Promise<void>;
-  abstract removePolicy(sec: string, ptype: string, rule: string[]): Promise<void>;
-  abstract removeFilteredPolicy(
-    sec: string,
-    ptype: string,
-    fieldIndex: number,
-    ...fieldValues: string[]
-  ): Promise<void>;
+
+  // -----------------------------------------------------------------------------------------
+  isFiltered(): boolean {
+    return true;
+  }
+
+  // -----------------------------------------------------------------------------------------
+  loadPolicy(_: Model): Promise<void> {
+    return Promise.resolve();
+  }
+
+  // -----------------------------------------------------------------------------------------
+  savePolicy(model: Model): Promise<boolean> {
+    this.logger.info('[savePolicy] Ignore save policy method with options: ', { model });
+    return Promise.resolve(true);
+  }
+
+  // -----------------------------------------------------------------------------------------
+  addPolicy(sec: string, ptype: string, rule: string[]): Promise<void> {
+    this.logger.info('[addPolicy] Ignore add policy method with options: ', { sec, ptype, rule });
+    return Promise.resolve();
+  }
+
+  // -----------------------------------------------------------------------------------------
+  removePolicy(sec: string, ptype: string, rule: string[]): Promise<void> {
+    this.logger.info('[removePolicy] Ignore remove policy method with options: ', { sec, ptype, rule });
+    return Promise.resolve();
+  }
+
+  // -----------------------------------------------------------------------------------------
+  removeFilteredPolicy(sec: string, ptype: string, fieldIndex: number, ...fieldValues: string[]): Promise<void> {
+    switch (ptype) {
+      case EnforcerDefinitions.PREFIX_USER: {
+        // Remove user policy
+        break;
+      }
+      case EnforcerDefinitions.PREFIX_ROLE: {
+        // Remove role policy
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+
+    this.logger.info('[removeFilteredPolicy] Ignore remove filtered policy method with options: ', {
+      sec,
+      ptype,
+      fieldIndex,
+      fieldValues,
+    });
+    return Promise.resolve();
+  }
 }
