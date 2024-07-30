5665a33486c47734d103f89e1ba660b2d262790d
Phat Nguyen <tanphat199@gmail.com>
2024-07-30 14:58:45 +0700

    export authorize adapter (#34)

--- dist/components/authorize/index.js
+++ dist/components/authorize/index.js
@@ -14,10 +14,12 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./adapters"), exports);
 __exportStar(require("./component"), exports);
 __exportStar(require("./decorator"), exports);
 __exportStar(require("./models"), exports);
 __exportStar(require("./provider"), exports);
 __exportStar(require("./repositories"), exports);
 __exportStar(require("./services"), exports);
+__exportStar(require("./types"), exports);
 //# sourceMappingURL=index.js.map
