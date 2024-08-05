ebc1be93034f907305185cc54988322958631419
Phat Nguyen <tanphat199@gmail.com>
2024-08-05 10:20:17 +0700

    refactor migration method to up and down (#47)

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -20,12 +20,10 @@ export const cleanUpMigration = async (application: BaseApplication, migrationPr
       logger.error('[%s] FAILED | Clean up migrate process | Error: %s', name, error);
     }
   }
-
-  logger.info('DONE | Clean up migrate database');
 };
 
-export const migration = async (application: BaseApplication, migrationProcesses: Array<MigrationProcess>) => {
-  logger.info('START | Migrate database');
+export const _migration = async (application: BaseApplication, migrationProcesses: Array<MigrationProcess>) => {
+  logger.info('[migration] START Migrating database');
   const migrationRepository = application.getSync<MigrationRepository>('repositories.MigrationRepository');
 
   for (const migrationProcess of migrationProcesses) {
@@ -38,25 +36,23 @@ export const migration = async (application: BaseApplication, migrationProcesses
     let migrateStatus: string = MigrationStatuses.UNKNOWN;
 
     try {
-      migrated = await migrationRepository.findOne({
-        where: { name },
-      });
+      migrated = await migrationRepository.findOne({ where: { name } });
 
       if (!options?.alwaysRun && migrated && migrated.status === MigrationStatuses.SUCCESS) {
         migrateStatus = migrated.status;
-        logger.info('[%s] SKIP | Migrate process', name);
+        logger.info('[migration] SKIP | Migrate process: %s | status: %s', name, migrateStatus);
         continue;
       }
 
-      logger.info('[%s] START | Migrate process', name);
+      logger.info('[migration] START | Migrate process: %s', name);
 
       await fn(application);
       migrateStatus = MigrationStatuses.SUCCESS;
 
-      logger.info('[%s] DONE | Migrate process', name);
+      logger.info('[migration] DONE | Migrate process: %s', name);
     } catch (error) {
       migrateStatus = MigrationStatuses.FAIL;
-      logger.error('[%s] FAILED | Migrate process | Error: %s', name, error);
+      logger.error('[migration] FAILED | Migrate process: %s | Error: %s', name, error);
     } finally {
       if (migrated) {
         await migrationRepository.updateById(migrated.id as number, { status: migrateStatus });
@@ -66,5 +62,18 @@ export const migration = async (application: BaseApplication, migrationProcesses
     }
   }
 
-  logger.info('DONE | Migrate database');
+  logger.info('[migration] DONE Migrating database');
+};
+
+export const migration = _migration;
+
+export const migrate = async (opts: {
+  scope: 'up' | 'down';
+  application: BaseApplication;
+  processes: Array<MigrationProcess>;
+}) => {
+  const { scope, application, processes } = opts;
+  logger.info('[%s] START | Migrating database', scope.toUpperCase());
+  await _migration(application, processes);
+  logger.info('[%s] DONE | Migrating database', scope.toUpperCase());
 };
