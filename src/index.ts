cb85fd72fc0c1b8ad06d2f0acd930fb74bf30577
Phat Nguyen <tanphat199@gmail.com>
2024-08-25 10:39:55 +0700

    add logger

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { applicationLogger } from './helpers';
 const {
   NODE_ENV,
   RUN_MODE,
+  ALLOW_EMPTY_ENV_VALUE = false,
+  APPLICATION_ENV_PREFIX = 'APP_ENV',
+
   APP_ENV_APPLICATION_NAME = 'PNT',
   APP_ENV_APPLICATION_TIMEZONE = 'Asia/Ho_Chi_Minh',
   APP_ENV_DS_MIGRATION = 'postgres',
@@ -10,14 +13,20 @@ const {
   APP_ENV_LOGGER_FOLDER_PATH = './',
 } = process.env;
 
-applicationLogger.info('------------------------------------');
+applicationLogger.info('------------------------------------------------------------------------');
 applicationLogger.info('Application configures:');
-applicationLogger.info('- Env: %s | Run mode: %s', NODE_ENV, RUN_MODE);
+applicationLogger.info(
+  '- Env: %s | AllowEmptyEnv: %s | Prefix: %s | Run mode: %s',
+  NODE_ENV,
+  ALLOW_EMPTY_ENV_VALUE,
+  APPLICATION_ENV_PREFIX,
+  RUN_MODE,
+);
 applicationLogger.info('- Name: %s', APP_ENV_APPLICATION_NAME);
 applicationLogger.info('- Timezone: %s', APP_ENV_APPLICATION_TIMEZONE);
 applicationLogger.info('- LogPath: %s', APP_ENV_LOGGER_FOLDER_PATH);
 applicationLogger.info('- MigrationDS: %s | AuthorizeDS: %s', APP_ENV_DS_MIGRATION, APP_ENV_DS_AUTHORIZE);
-applicationLogger.info('------------------------------------');
+applicationLogger.info('------------------------------------------------------------------------');
 
 export * from './base';
 export * from './common';
