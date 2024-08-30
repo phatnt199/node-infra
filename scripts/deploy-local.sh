af08bb85bebb43b55e720b368fc556896ec07953
Phat Nguyen <tanphat199@gmail.com>
2024-08-30 12:25:13 +0700

    fix deployment local script

--- scripts/deploy-local.sh
+++ scripts/deploy-local.sh
@@ -3,8 +3,8 @@ echo "Rebuilt sources"
 path_project=$1
 
 echo "Deploy to project $path_project"
-rm -rf $path_project/node_modules/@minimaltech/lb-infra/dist
+rm -rf $path_project/node_modules/@minimaltech/node-infra/dist
 echo "Clean up old sources"
 
-mv dist $path_project/node_modules/@minimaltech/lb-infra
+mv dist $path_project/node_modules/@minimaltech/node-infra
 echo "Deployed sources"
