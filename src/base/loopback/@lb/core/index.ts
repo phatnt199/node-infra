2f14771db5affdb6239cfae9daf41e79d2883e26
Phat Nguyen <tanphat199@gmail.com>
2024-09-18 14:49:18 +0700

    add @lb named export

---
+++ src/base/loopback/@lb/core/index.ts
@@ -0,0 +1,6 @@
+export * from './../../boot';
+export * from './../../core';
+export * from './../../http-server';
+export * from './../../metadata';
+export * from './../../security';
+export * from './../../service-proxy';
