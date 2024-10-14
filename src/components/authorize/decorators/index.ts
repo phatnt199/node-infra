0e3e1f70b3f9fd5826fd1e970c37d6b31c5bf457
Chung Phùng Phát <dev@chphungphat.com>
2024-10-14 08:57:20 +0000

    Fix generator service to correctly generate permission methods inherited from parent class (#68)
    * change husky to use pnpm
    
    * add parseArrayToRecord and parseArrayToMap utility function
    
    * fetch whole file and remove objects static asset
    
    * add console.log  temporarily to debug
    
    * add table name to view authorize query
    
    * fix generatePermissionService
    
    * generate permission now correctly get all parent methods
    
    * remove allowRole and denyRole in permission decorator

--- src/components/authorize/decorators/index.ts
+++ src/components/authorize/decorators/index.ts
@@ -9,6 +9,17 @@ export interface IPermissionDecorator {
 }
 
 // Read more: https://loopback.io/doc/en/lb4/Creating-decorators.html#inspect-metadata-of-a-property
+/**
+ * @example
+ * ```typescript
+ * class MyController {
+ *   @permission({ idx: 1, allowedRoles: ['admin'], deniedRoles: ['guest'] })
+ *   myMethod() {
+ *     // method implementation
+ *   }
+ * }
+ * ```
+ */
 export function permission(spec: IPermissionDecorator): MethodDecorator {
   return MethodDecoratorFactory.createDecorator<IPermissionDecorator>(MetadataDecoratorKeys.PERMISSION, spec);
 }
