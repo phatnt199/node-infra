83e9fcf3eeabc80f9dc46a6eadf8e9dd9c3c4a01
Cao Chi Hai <56874076+haicao2805@users.noreply.github.com>
2025-03-02 21:21:33 +0700

    Haicao (#73)
    * add [DataTypeMixin]: boolean type column
    
    * v0.1.11
    
    * feat: api /count for relational controller + cleanUpMigrate function
    
    * v0.1.22
    
    * v0.1.23
    
    * refactor: mixin data type
    
    * v0.1.24
    
    * refactor: searchable repository
    
    * refactor: searchable repo
    
    * add: update builder knex
    
    * fix [SearchableTzRepository]: add check array length
    
    * fix [SearchableTzRepository]: add check array length
    
    * fix [ContentRange]: fix bug set wrong content-range for has many (through) controller
    
    * add: after deleteWithReturn hook
    
    * add: after deleteWithReturn hook
    
    * fix [SearchableTzRepository]: fix renderSearchable function
    
    * chore: change return type of api findOne
    
    * fix [SearchableTzRepository]: check null before update object search
    
    * fix [SearchableTzRepository]: missing options in registerOnInclusionChanged
    
    * fix: missing options on searchableTzRepository
    
    * fix: missing options on searchableTzRepository
    
    * add [SoftDeleteRepository]: add return type Count to softDelete
    
    * fix [ObjectSearchTzRepository]: mixin user audit before mix searchable fields
    
    * add [SearchableTzCrudRepository]: syncSearchFields function
    
    * adjust [SearchableTzCrudRepository]: use batch update for fn syncSearchFields
    
    * add: batchUpdate
    
    * add: batchUpdate
    
    * add: batchUpdate
    
    * add: batchUpdate
    
    * add: batchUpdate
    
    * add: batchUpdate
    
    * add: batchUpdate
    
    * add: batchUpdate
    
    * adjust [SocketIOClientHelper]: allow to ignore duplicate handler
    
    * adjust [SocketIOClientHelper]: allow to ignore duplicate handler

--- src/components/socket-io/helpers/socket-io-client.helper.ts
+++ src/components/socket-io/helpers/socket-io-client.helper.ts
@@ -51,8 +51,9 @@ export class SocketIOClientHelper {
   }
 
   // -----------------------------------------------------------------
-  subscribe(opts: { events: Record<string, (...props: any) => void> }) {
-    const eventHandlers = opts.events;
+  subscribe(opts: { events: Record<string, (...props: any) => void>; ignoreDuplicate?: boolean }) {
+    const { events: eventHandlers, ignoreDuplicate = false } = opts;
+
     const eventNames = Object.keys(eventHandlers);
     this.logger.info('[subscribe][%s] Handling events: %j', this.identifier, eventNames);
 
@@ -61,6 +62,15 @@ export class SocketIOClientHelper {
       if (!handler) {
         this.logger.info(
           '[subscribe][%s] Ignore handling event %s because of no handler!',
+          this.identifier,
+          eventName,
+        );
+        continue;
+      }
+
+      if (ignoreDuplicate && this.client.hasListeners(eventName)) {
+        this.logger.info(
+          '[subscribe][%s] Ignore handling event %s because of duplicate handler!',
           this.identifier,
           eventName,
         );
